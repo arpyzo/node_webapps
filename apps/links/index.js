24d007b7487e9018187ec7940b0703295ccf3de2
Robert Pyzalski <pyzals@gmail.com>
2020-11-29 15:31:20 -0800

    converting apps to classes

--- apps/links/index.js
+++ apps/links/index.js
@@ -1,111 +1,113 @@
 const fs = require("fs");
 
-function getCategoryFile(category) {
-    return "/Users/robert/Temp/links/" + category;
-}
-
-function handle(request, requestData, response) {
-    console.log(`App links will handle ${request.url}`);
-
-    if (/^\/[a-z]*$/.test(request.url)) {
-        return response.returnHTML(fs.readFileSync(__dirname + "/view/index.html"));
-    } 
-
-    if (request.url.startsWith("/js/")) {
-        return response.returnJS(fs.readFileSync(__dirname + "/view" + request.url));
-    } 
-
-    if (request.url.startsWith("/css/")) {
-        return response.returnCSS(fs.readFileSync(__dirname + "/view" + request.url));
-    }
-    
-    if (request.url == "/api/load") {
-        let category = request.queryParams.get("category");
-        if (!category) {
-           return response.return400("Missing category parameter");
-        }
-
-        if (!doLinksExist(category)) {
-            return response.return404();
-        }
-
-        try {
-            return response.returnText(getLinks(category));
-        } catch(error) {
-            console.trace(`Error loading links: ${error}`);
-            return response.return500(error);
-        }
+class Links {
+    constructor(saveDir) {
+        this.linksDir = saveDir + "links/";
     }
 
-    if (request.url == "/api/append") {
-        try {
-            var appendData = JSON.parse(requestData);
-        } catch(error) {
-            return response.return400(error);
+    handle(request, requestData, response) {
+        console.log(`App links will handle ${request.url}`);
+
+        if (/^\/[a-z]*$/.test(request.url)) {
+            return response.returnHTML(fs.readFileSync(__dirname + "/view/index.html"));
+        } 
+
+        if (request.url.startsWith("/js/")) {
+            return response.returnJS(fs.readFileSync(__dirname + "/view" + request.url));
+        } 
+
+        if (request.url.startsWith("/css/")) {
+            return response.returnCSS(fs.readFileSync(__dirname + "/view" + request.url));
+        }
+        
+        if (request.url == "/api/load") {
+            let category = request.queryParams.get("category");
+            if (!category) {
+               return response.return400("Missing category parameter");
+            }
+
+            if (!this.doLinksExist(category)) {
+                return response.return404();
+            }
+
+            try {
+                return response.returnText(this.getLinks(category));
+            } catch(error) {
+                console.trace(`Error loading links: ${error}`);
+                return response.return500(error);
+            }
         }
 
-        if (!appendData.category || !appendData.link) {
-            return response.return400("Missing or empty category and/or link parameter");
-        }
+        if (request.url == "/api/append") {
+            try {
+                var appendData = JSON.parse(requestData);
+            } catch(error) {
+                return response.return400(error);
+            }
 
-        if (!doLinksExist(appendData.category)) {
-            return response.return404();
-        }
+            if (!appendData.category || !appendData.link) {
+                return response.return400("Missing or empty category and/or link parameter");
+            }
 
-        try {
-            appendLink(appendData.category, appendData.link);
-            return response.return200();
-        } catch(error) {
-            console.trace(`Error appending link: ${error}`);
-            return response.return500(error);
+            if (!this.doLinksExist(appendData.category)) {
+                return response.return404();
+            }
+
+            try {
+                this.appendLink(appendData.category, appendData.link);
+                return response.return200();
+            } catch(error) {
+                console.trace(`Error appending link: ${error}`);
+                return response.return500(error);
+            }
         }
+     
+        if (request.url == "/api/remove") {
+            try {
+                var appendData = JSON.parse(requestData);
+            } catch(error) {
+                return response.return400(error);
+            }
+
+            if (!appendData.category || !appendData.link) {
+                return response.return400("Missing or empty category and/or link parameter");
+            }
+
+            if (!this.doLinksExist(appendData.category)) {
+                return response.return404();
+            }
+
+            try {
+                this.removeLink(appendData.category, appendData.link);
+                return response.return200();
+            } catch(error) {
+                console.trace(`Error removing link: ${error}`);
+                return response.return500(error);
+            }
+        }
+     
+        response.return404();
     }
- 
-    if (request.url == "/api/remove") {
-        try {
-            var appendData = JSON.parse(requestData);
-        } catch(error) {
-            return response.return400(error);
-        }
 
-        if (!appendData.category || !appendData.link) {
-            return response.return400("Missing or empty category and/or link parameter");
-        }
-
-        if (!doLinksExist(appendData.category)) {
-            return response.return404();
-        }
-
-        try {
-            removeLink(appendData.category, appendData.link);
-            return response.return200();
-        } catch(error) {
-            console.trace(`Error removing link: ${error}`);
-            return response.return500(error);
-        }
+    doLinksExist(category) {
+        return fs.existsSync(this.linksDir + category);
     }
- 
-    response.return404();
+
+    getLinks(category) {
+        return fs.readFileSync(this.linksDir + category);
+    }
+
+    appendLink(category, link) {
+        fs.appendFileSync(this.linksDir + category, link + "\n");
+    }
+
+    removeLink(category, link) {
+        let links = fs.readFileSync(this.linksDir + category, 'utf8').split("\n");
+        let newLinks = links.filter(function(line) {
+            return line != link;
+        });
+        fs.writeFileSync(this.linksDir + category, newLinks.join("\n"));
+    }
 }
 
-function doLinksExist(category) {
-    return fs.existsSync(getCategoryFile(category));
-}
-
-function getLinks(category) {
-    return fs.readFileSync(getCategoryFile(category));
-}
-
-function appendLink(category, link) {
-    fs.appendFileSync(getCategoryFile(category), link + "\n");
-}
-
-function removeLink(category, link) {
-    let links = fs.readFileSync(getCategoryFile(category), 'utf8').split("\n");
-    let newLinks = links.filter(function(line) {
-        return line != link;
-    });
-    fs.writeFileSync(getCategoryFile(category), newLinks.join("\n"));
-}
-
-exports.handle = handle;
+exports.app = Links;
--- index.js
+++ index.js
@@ -2,11 +2,12 @@ const server = require("./server");
 
 const port = 8080;
 const apps = {};
-const appsDir = "./apps";
+const appsDir = "./apps/";
+const saveDir = "/Users/robert/Temp/webapps/";
 
 require("fs").readdirSync(appsDir, { withFileTypes: true}).forEach(function(appDirEntry) {
     if (appDirEntry.isDirectory()) {
-        apps[appDirEntry.name] = require(appsDir + "/" + appDirEntry.name);
+        apps[appDirEntry.name] = new (require(appsDir + appDirEntry.name).app)(saveDir);
     }
 });
 
