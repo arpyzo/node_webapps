d87bf983524d680e13f1ca73b5bdb96dd310c9e2
Robert Pyzalski <pyzals@gmail.com>
2023-11-27 17:43:49 -0800

    [weights] expanding layout, adding api/load

--- apps/weights/index.js
+++ apps/weights/index.js
@@ -1,12 +1,29 @@
 const fs = require("fs");
 
 class Weights {
+   constructor(config) {
+        this.saveDir = config.saveDir;
+    }
+
     handle(request, response) {
         if (request.url == "/") {
             return response.returnAsset(__dirname + "/view/index.html");
         }
 
+        if (request.url == "/api/load") {
+            try {
+                return response.returnJSON(this.getExercises());
+            } catch(error) {
+                console.trace(`Error loading exercises: ${error}`);
+                return response.return500(error);
+            }
+        }
+
         response.return404();
+    }
+
+    getExercises() {
+        return fs.readFileSync(`${this.saveDir}weights.json`);
     }
 }
 
